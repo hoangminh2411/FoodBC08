7772936cbde33ecb2f877ee7475f78e13bbeb583
macbook <khaitruong1301@gmail.com>
2021-07-07 22:07:12 +0700

    es6_quanLyMenu

--- app/controllers/food-list.js
+++ app/controllers/food-list.js
@@ -1,15 +1,71 @@
+import { Menu } from "../models/Menu.js";
+import { MonAn } from "../models/MonAn.js";
 
-let arrMonAn = [];
-//Lấy dữ liệu từ localstorage ra arrMonAn
-let getDataStorage = () => {
+// let arrMonAn = [];
 
-    if(localStorage.getItem('danhSachMonAn')){
+let menu = new Menu();
+menu.layStorage();
+menu.renderMenu('tbodyFood');
 
-        arrMonAn = JSON.parse(localStorage.getItem('danhSachMonAn'))
+
+
+window.xoaMonAn = function (maMon) {
+    
+    menu.xoaMonAn(maMon);
+    menu.renderMenu('tbodyFood');
+    menu.luuStorage();
+}
+
+
+window.chinhSua = function (maMon) {
+    //Trong hàm này xử lý load thông tin món ăn có mã này lên giao diện
+    let monAn = menu.layThongTinMonAn(maMon);
+
+    // document.querySelector('#maMon').value = monAn.maMon;
+    // document.querySelector('#tenMon').value = monAn.tenMon;
+
+
+
+    if(monAn) {
+        //Load dữ liệu lên các input của popup modal
+        console.log('monAn',monAn)
+        //{maMon:1,tenMon:'Cơm chiên', giaMon:'',...}
+
+        //Sau khi dom trả về các taginput được ảnh hưởng bởi selector này
+        let arrInput = document.querySelectorAll('#foodForm input,#foodForm select, #foodForm textarea');
+
+        for(let input of arrInput) {
+            //{id,className,name,value} = input;
+            let name = input.getAttribute('name');
+            input.value = monAn[name]; 
+        }
+
     }
 }
 
-getDataStorage();
+document.querySelector('#btnCapNhat').onclick =  () => {
+
+    let monAnCapNhat = new MonAn();
+
+    // monAnCapNhat.maMon = document.querySelector('#maMon').value;
+    let arrInput = document.querySelectorAll('#foodForm input,#foodForm select, #foodForm textarea');
+    for(let input of arrInput) {
+        let name = input.getAttribute('name');
+        let value = input.value;
+
+        monAnCapNhat[name] = value;
+    }
+
+    menu.capNhatMonAn(monAnCapNhat.maMon,monAnCapNhat);
+
+    menu.renderMenu('tbodyFood');
+    //Tắt popup
+    document.querySelector('.btn-secondary').click();
+    menu.luuStorage();
+
+}
 
 
-console.log('arr',arrMonAn);
+
+// Object.thuocTinh
+// object[thuocTinh]
